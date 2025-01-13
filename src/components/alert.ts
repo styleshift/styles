b02b9b0b8fd497379e69c30ffa89a0fdc458b080
Jeroen Gerits <jeroen@gerits.email>
2025-01-13 19:05:27 +0100

    wip publish

--- src/components/alert.ts
+++ src/components/alert.ts
@@ -2,37 +2,12 @@ import styleshift from '../styleshift';
 
 export const alert = {
   root: styleshift.define({
-    base: [
-      'relative',
-      'w-full',
-      'rounded-lg',
-      'border',
-      'p-4',
-      'transition-all',
-      '[&>svg~*]:pl-7',
-      '[&>svg+div]:translate-y-[-3px]',
-      '[&>svg]:absolute',
-      '[&>svg]:left-4',
-      '[&>svg]:top-4',
-      '[&>svg]:text-foreground',
-    ],
+    base: ['relative', 'w-full', 'rounded-lg', 'border', 'p-4', 'transition-all'],
     variants: {
       surface: {
         default: ['bg-background', 'text-foreground', 'border-border'],
-        negative: [
-          'border-red-200',
-          'text-red-500',
-          'dark:border-red-400',
-          '[&>svg]:text-red-500',
-          'dark:[&>svg]:text-red-400',
-        ],
-        positive: [
-          'border-green-200',
-          'text-green-700',
-          'dark:text-green-400',
-          '[&>svg]:text-green-700',
-          'dark:[&>svg]:text-green-400',
-        ],
+        negative: ['border-red-200', 'text-red-500'],
+        positive: ['border-green-200', 'text-green-700'],
       },
     },
     defaultVariants: {
