c1221cd5d0d45722ceb5ade77eef68264a997061
Jeroen Gerits <jeroen@gerits.email>
2025-01-13 15:13:34 +0100

    wip

--- src/components/states/canDisable.ts
+++ src/components/states/canDisable.ts
@@ -1,4 +1,4 @@
-import { cva, type VariantProps } from '../../shared';
+import { cva } from '../../shared';
 
 export const canDisable = cva({
   base: ['disabled:opacity-50', 'disabled:cursor-not-allowed', 'disabled:pointer-events-none'],
--- src/components/states/canFocus.ts
+++ src/components/states/canFocus.ts
@@ -1,4 +1,4 @@
-import { cva, type VariantProps } from '../../shared';
+import { cva } from '../../shared';
 
 export const canFocus = cva({
   base: ['focus:outline-none', 'focus:ring-2', 'focus:ring-offset-2', 'focus:ring-slate-600'],
